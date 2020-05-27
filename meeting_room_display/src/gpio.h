1c048e6f90c99de51e7274c6af20cb7f5f9de96c
Tobias Schaffner <tobiasschaffner87@outlook.com>
2020-05-27 11:20:27 +0200

    Only configure buttons for development kit

--- meeting_room_display/src/gpio.h
+++ meeting_room_display/src/gpio.h
@@ -1,9 +1,12 @@
 #ifndef GPIO_H
 #define GPIO_H
 
+#if defined(BOARD_NRF52840_PCA10056)
 int gpio_init(void);
 
 // Callbacks for buttons to be defined by user.
 void on_button_press(u32_t button);
-
+#else
+int gpio_init(void) { return 0; }
+#endif
 #endif
