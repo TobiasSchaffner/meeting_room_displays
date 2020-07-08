b5532ee2fd86680ee72a441207b61722ed0797c4
Tobias Schaffner <tobiasschaffner87@outlook.com>
2020-07-08 17:36:30 +0200

    Send device to low power state

--- meeting_room_display/src/power.c
+++ meeting_room_display/src/power.c
@@ -12,7 +12,7 @@ bool initialized = false;
 static void disable_device(const char* name)
 {
 	struct device *dev = device_get_binding(name);
-	int rc = device_set_power_state(dev, DEVICE_PM_OFF_STATE, NULL, NULL);
+	int rc = device_set_power_state(dev, DEVICE_PM_LOW_POWER_STATE, NULL, NULL);
 
 	if (rc) {
 		printk("Error disabling device %s peripheral (%d)\n", name, rc);
@@ -29,46 +29,18 @@ static void enable_device(const char* name)
 	}
 }
 
-/*
- * Application defined function for doing any target specific operations
- * for low power entry.
- */
-void sys_pm_notify_power_state_entry(enum power_states state)
-{
+bool sys_pm_policy_low_power_devices(enum power_states state) {
 	if (initialized) {
 		switch (state) {
 		case SYS_POWER_STATE_SLEEP_1:
 			printk("Disabling devices\n");
-			//disable_device("UART_O");
 			disable_device("UART_1");
-			k_cpu_idle();
-			break;
+			return true;
 		default:
 			printk("Unsupported power state %u\n", state);
-			break;
 		}
 	}
-}
-
-/*
- * Application defined function for doing any target specific operations
- * for low power exit.
- */
-void sys_pm_notify_power_state_exit(enum power_states state)
-{
-	if (initialized) {
-		switch (state) {
-		case SYS_POWER_STATE_SLEEP_1:
-			//enable_device("UART_O");
-			enable_device("UART_1");
-			printk("Enabled devices\n");
-			break;
-
-		default:
-			printk("Unsupported power state %u\n", state);
-			break;
-		}
-	}
+	return false;
 }
 
 int power_init(void) {
